4fc9ed247f271d8a810f601f857669585c38d860
ManjushaR92 <97123266+ManjushaR92@users.noreply.github.com>
2022-03-21 16:11:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.get('/',function(req,res){
     
 });
 
-app.listen(process.env.PORT||3000, ()=>{
-    console.log("Server Ready on 3000");
+app.listen(process.env.PORT||8080, ()=>{
+    console.log("Server Ready on 8080");
 });
